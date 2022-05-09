0105b43a32eb1fcf76f4834fb6ca2e75b3b499f3
Ohad <ohadmil@post.bgu.ac.il>
2022-05-09 17:59:42 +0300

    add borders, fix amount of balls. add dialog of end game.

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var pac_color;
 var start_time;
 var time_elapsed;
 var interval;
-var gameOn=true;
+var gameOn;
+var ballLeft;
 
 // var upKey = 38;
 // var downKey = 40;
@@ -17,64 +18,92 @@ var gameOn=true;
 // var gameTime = 70;
 // var monsterAmount = 1;
 
-$(document).ready(function() {
+$(document).ready(function () {
 	context = canvas.getContext("2d");
-	
 });
 
 function Start() {
+	console.log("start game")
+	gameOn = true;
+	timeLeft = gameTime
 	board = new Array();
 	score = 0;
 	pac_color = "yellow";
-	var cnt = 100;
+	ballLeft = ballsAmount;
+	var cnt = 100;// ?
 	var food_remain = 50;
+	var smallFood = Math.floor(ballsAmount * 0.6);
+	var largeFood = Math.floor(ballsAmount * 0.3);
+	var bigFood = Math.floor(ballsAmount * 0.1);
 	var pacman_remain = 1;
 	start_time = new Date();
-	for (var i = 0; i < 10; i++) {
+
+	for (var i = 0; i < 20; i++) {
 		board[i] = new Array();
 		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
-		for (var j = 0; j < 10; j++) {
-			if (
-				(i == 3 && j == 3) ||
-				(i == 3 && j == 4) ||
-				(i == 3 && j == 5) ||
-				(i == 6 && j == 1) ||
-				(i == 6 && j == 2)
+		for (var j = 0; j < 20; j++) {
+			if (i == 0 || j == 0) {
+				continue;
+			}
+			if (//put obstacles
+				(i == 2 && j == 3) || (i == 3 && j == 3) || (i == 4 && j == 3) ||
+				(i == 2 && j == 4) || (i == 2 && j == 5) || (i == 2 && j == 6)
 			) {
-				board[i][j] = 4; // wall
+				board[i][j] = 4; // 4 -> wall
 			} else {
 				var randomNum = Math.random();
-				if (randomNum <= (1.0 * food_remain) / cnt) { // food
-					food_remain--;
-					board[i][j] = 1;
-				} else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) { // 2->pacman
+				if (pacman_remain != 0 && randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) { // 2-> pacman
 					shape.i = i;
 					shape.j = j;
 					pacman_remain--;
 					board[i][j] = 2;
-				} else { //empty 
+				} else { //0 -> empty 
 					board[i][j] = 0;
 				}
 				cnt--;
 			}
 		}
 	}
-	while (food_remain > 0) {
+
+	for (var i = 0; i < 20; i++) {
+		board[i][0] = 4;
+		board[i][19] = 4;
+	}
+	for (var i = 0; i < 20; i++) {
+		board[0][i] = 4;
+		board[19][i] = 4;
+	}
+
+	console.log(smallFood, largeFood, bigFood);
+	while (smallFood > 0 && food_remain > 0) {
 		var emptyCell = findRandomEmptyCell(board);
-		board[emptyCell[0]][emptyCell[1]] = 1; //1 -> food
+		board[emptyCell[0]][emptyCell[1]] = 11; //11 -> food small
 		food_remain--;
+		smallFood--;
+	}
+	while (largeFood > 0 && food_remain > 0) {
+		var emptyCell = findRandomEmptyCell(board);
+		board[emptyCell[0]][emptyCell[1]] = 12; //12 -> food large
+		food_remain--;
+		largeFood--;
+	}
+	while ( bigFood > 0 && food_remain > 0) {
+		var emptyCell = findRandomEmptyCell(board);
+		board[emptyCell[0]][emptyCell[1]] = 13; //13 -> food Big
+		food_remain--;
+		bigFood--;
 	}
 	keysDown = {};
 	addEventListener(
 		"keydown",
-		function(e) {
+		function (e) {
 			keysDown[e.keyCode] = true;
 		},
 		false
 	);
 	addEventListener(
 		"keyup",
-		function(e) {
+		function (e) {
 			keysDown[e.keyCode] = false;
 		},
 		false
@@ -83,15 +112,14 @@ function Start() {
 }
 
 function findRandomEmptyCell(board) {
-	var i = Math.floor(Math.random() * 9 + 1);
-	var j = Math.floor(Math.random() * 9 + 1);
+	var i = Math.floor(Math.random() * 19 + 1);
+	var j = Math.floor(Math.random() * 19 + 1);
 	while (board[i][j] != 0) {
-		i = Math.floor(Math.random() * 9 + 1);
-		j = Math.floor(Math.random() * 9 + 1);
+		i = Math.floor(Math.random() * 19 + 1);
+		j = Math.floor(Math.random() * 19 + 1);
 	}
 	return [i, j];
 }
-
 
 function GetKeyPressed() {
 	// console.log(keysDown)
@@ -108,22 +136,22 @@ function GetKeyPressed() {
 		return 4;
 	}
 }
-function endGame(){
-	gameOn= false;
-
+function endGame() {
+	gameOn = false;
+	ShowDialogGameOver();
 }
 
 function Draw() {
 	canvas.width = canvas.width; //clean board
 	lblScore.value = score;
-	timeLeft = Math.floor(gameTime -time_elapsed);
+	timeLeft = Math.floor(gameTime - time_elapsed);
 	lblTime.value = timeLeft;
-	if(timeLeft<= 0){
-		alert("end of time");
+	if (timeLeft <= 0) {
+		console.log("end of time");
 		endGame();
 	}
-	for (var i = 0; i < 10; i++) {
-		for (var j = 0; j < 10; j++) {
+	for (var i = 0; i < 20; i++) {
+		for (var j = 0; j < 20; j++) {
 			var center = new Object();
 			center.x = i * 60 + 30;
 			center.y = j * 60 + 30;
@@ -137,12 +165,26 @@ function Draw() {
 				context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
 				context.fillStyle = "black"; //color
 				context.fill();
-			} else if (board[i][j] == 1) { //draw food
+			}
+
+			else if (board[i][j] == 11) { //draw food
 				context.beginPath();
 				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
-				context.fillStyle = "black"; //color
+				context.fillStyle = threeColors[0]; //color
 				context.fill();
-			} else if (board[i][j] == 4) { // wall
+			} else if (board[i][j] == 12) { //draw food
+				context.beginPath();
+				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
+				context.fillStyle = threeColors[1]; //color
+				context.fill();
+			} else if (board[i][j] == 13) { //draw food
+				context.beginPath();
+				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
+				context.fillStyle = threeColors[2]; //color
+				context.fill();
+			}
+
+			else if (board[i][j] == 4) { // wall
 				context.beginPath();
 				context.rect(center.x - 30, center.y - 30, 60, 60);
 				context.fillStyle = "grey"; //color
@@ -153,43 +195,51 @@ function Draw() {
 }
 
 function UpdatePosition() {
-	if(gameOn){
-	board[shape.i][shape.j] = 0;
-	var x = GetKeyPressed();
-	if (x == 1) {// up
-		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
-			shape.j--;
+	if (gameOn) {
+		board[shape.i][shape.j] = 0;
+		var x = GetKeyPressed();
+		if (x == 1) {// up
+			if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
+				shape.j--;
+			}
+		}
+		if (x == 2) { // down
+			if (shape.j < 19 && board[shape.i][shape.j + 1] != 4) {
+				shape.j++;
+			}
+		}
+		if (x == 3) { // left
+			if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
+				shape.i--;
+			}
+		}
+		if (x == 4) {//right
+			if (shape.i < 19 && board[shape.i + 1][shape.j] != 4) {
+				shape.i++;
+			}
+		}
+
+		if (board[shape.i][shape.j] == 11) {
+			score += 5; ballLeft--;
+		}
+		if (board[shape.i][shape.j] == 12) {
+			score += 15; ballLeft--;
+		}
+		if (board[shape.i][shape.j] == 13) {
+			score += 25; ballLeft--;
+		}
+		board[shape.i][shape.j] = 2;
+		var currentTime = new Date();
+		time_elapsed = (currentTime - start_time) / 1000;
+		// if (score >= 20 && time_elapsed <= 10) {
+		// 	pac_color = "green";
+		// }	
+		if (ballLeft == 0) {
+			console.log("game over......");
+			window.clearInterval(interval);
+			window.alert("Game completed");
+		} else {
+			Draw();
 		}
 	}
-	if (x == 2) { // down
-		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
-			shape.j++;
-		}
-	}
-	if (x == 3) { // left
-		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
-			shape.i--;
-		}
-	}
-	if (x == 4) {//right
-		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
-			shape.i++;
-		}
-	}
-	if (board[shape.i][shape.j] == 1) {
-		score++;
-	}
-	board[shape.i][shape.j] = 2;
-	var currentTime = new Date();
-	time_elapsed = (currentTime - start_time) / 1000;
-	if (score >= 20 && time_elapsed <= 10) {
-		pac_color = "green";
-	}
-	if (score == 50) {
-		window.clearInterval(interval);
-		window.alert("Game completed");
-	} else {
-		Draw();
-	}
-}
 }
