ba95bd10171f0428856ba97b5e0bccf97e372694
Ohad <ohadmil@post.bgu.ac.il>
2022-05-11 21:54:04 +0300

    add title and logo.

--- login.js
+++ login.js
@@ -3,10 +3,11 @@ localStorage.setItem("k", "k");
 function checkLogin() {
   user = document.getElementById("userLogin").value;
   password = document.getElementById("passwordLogin").value;
-  console.log("login: ",user, password);
+  console.log("login: ", user, password);
 
-  if (localStorage.getItem(user)== password) {
+  if (localStorage.getItem(user) == password) {
     console.log("login success");
+    loggedUser=user;
     changeDiv("settingPage");
   } else alert("Wrong input..");
 }
